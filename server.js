ca93c708fb82c2a6a60cc8f89315f16015fdde0f
MSAHARSHREDDY <saharshreddym59@gmail.com>
2025-05-05 07:52:43 +0530

    added changes

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ app.use("/api/products", (req, res) => {
   })
 });
 
+app.use("/api/products123",(req,res)=>{
+  return res.status(200).json({
+    message: 'This is new feature change, a new route for products123'
+  })
+})
+
 app.use(errorHandler);
 
 const server = app.listen(port, () =>
